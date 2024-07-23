5b203853c8761cd45b183ec94a522be97744586a
Dawid Kolano <dawid.kolano@gmail.com>
2024-07-23 18:59:37 +0200

    Initial commit.

---
+++ src/test/java/pl/dk/aibron_first_task/AibronFirstTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dk.aibron_first_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AibronFirstTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
